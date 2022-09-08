54b0ce98e26b3e2535a3fd6f636b2ea8b1631e45
archer-blackcat <39975354+archer-blackcat@users.noreply.github.com>
2022-09-08 08:45:42 +0800

    Update PPapp.py
    改下page title

--- PPapp.py
+++ PPapp.py
@@ -4,7 +4,7 @@ from PaiPan import paipan_csh,HePan
 import pandas as pd
 from st_aggrid import AgGrid
 
-pd.set_option('max_colwidth',100)
+st.set_page_config(layout="wide",page_title="嘿喵排盘")
 
 JieQi = ["冬至", "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏",
         "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑", "白露", "秋分", "寒露", "霜降",
