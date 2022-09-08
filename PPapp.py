2f904f69eab61b13900524ca7f4c8bfdb7bfbc4d
archer-blackcat <39975354+archer-blackcat@users.noreply.github.com>
2022-09-08 09:50:53 +0800

    Update PPapp.py
    时区修改
    
    Signed-off-by: archer-blackcat <39975354+archer-blackcat@users.noreply.github.com>

--- PPapp.py
+++ PPapp.py
@@ -16,8 +16,8 @@ def change6r():
     
 
 with st.sidebar:
-    pp_date=st.date_input("排盘日期",pdlm.now().date())
-    pp_time=st.time_input("排盘时间",pdlm.now().time())
+    pp_date=st.date_input("排盘日期",pdlm.now(tz='Asia/Shanghai').date())
+    pp_time=st.time_input("排盘时间",pdlm.now(tz='Asia/Shanghai').time())
     
     # btn1=st.button('排盘')
     # if btn1:
